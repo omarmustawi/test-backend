097e828f9f88aa88ec1d0fa4f2c6ea016df86015
omar mstawi <@zitsmartengineer@gmail.com>
2024-02-27 17:28:45 +0300

    front

--- src/pages/listposts/index.tsx
+++ src/pages/listposts/index.tsx
@@ -1,17 +1,23 @@
+"use client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function index() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [data, setData] = useState([{ title: "", content: "" }]);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     const fetchdata = async () => {
-        const res = await axios.get()
-    }
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API}`);
+      console.log("res", res);
+      setData(res.data)
+    };
+    fetchdata();
   }, []);
   return (
-    <div>
+    <div className="bg-red-300 grid gap-4 ">
       {data.map((ele, index) => (
-        <div key={index} className="">
+        <div key={index} className="grid gap-4  grid-cols-2 justify-center items-center bg-slate-400">
           <span>{ele.title}</span>
           <span>{ele.content}</span>
         </div>
