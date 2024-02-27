e367fcac93b57da69610dac730c259212de244b4
omar mstawi <@zitsmartengineer@gmail.com>
2024-02-27 14:37:33 +0300

    firt upload

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from  rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
